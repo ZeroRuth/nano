16fcc62ea53d5920a9aa41932b587993d7c4159c
ZeroRuth <75718744+ZeroRuth@users.noreply.github.com>
2021-01-18 02:54:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-#=================================================
-# 清除旧版argon主题并拉取最新版
 
-pushd package/lean
-
-rm -rf luci-theme-argon# luci-theme-argon for 18.06 by jerrykuku
+# luci-theme-argon for 18.06 by jerrykuku
 
 rm -rf package/lean/luci-theme-argon
 
