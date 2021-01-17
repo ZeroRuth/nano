8c84112cd4e859b95a202ad6a3a13a5552ec8a12
ZeroRuth <75718744+ZeroRuth@users.noreply.github.com>
2021-01-17 19:52:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,15 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
+.config
+git clone https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
+.config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon LUCI/Theme/Luci-theme-argon
+.config
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
+.config
+CONFIG_PACKAGE_lua-maxminddb=y
+CONFIG_PACKAGE_luci-app-vssr=y
+CONFIG_PACKAGE_Luci-theme-argon=y
+CONFIG_PACKAGE_luci-app-jd-dailybonus=y
