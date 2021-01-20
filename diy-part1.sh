b1752aeec263ee3fec5fc158d0dc65fd08aeccf4
ZeroRuth <75718744+ZeroRuth@users.noreply.github.com>
2021-01-20 14:54:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,6 @@
 sed -i 's/^#\(.*argon\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*argon-config\)/\1/' feeds.conf.default
 
-
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
